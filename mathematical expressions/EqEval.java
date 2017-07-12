2d1cde1ba0aa867310a905e3414c950beba79280
JKOK005 <JKOK005@e.ntu.edu.sg>
2017-07-13 00:40:51 +0800

    Added assertValidString method to EqEval main
    Asserts that an input sting is a valid string so that we do not need to
    raise any exceptions during the evaluation phase.
    
    Assertion is done by testing against negative cases such as
    - int int
    - oper oper
    - '(' oper
    - oper ')'
    These cases should flag an exception during run time

--- mathematical expressions/EqEval.java
+++ mathematical expressions/EqEval.java
@@ -21,17 +21,62 @@ public class EqEval{
 		return new_str.toString();
 	}
 
-	public int parseExpr(String line) throws Exception{
+	public boolean assertValidString(String line) throws Exception{
+		Stack<Character> stk = new Stack<Character>();
+		int flag = -1;
+		char curr, tmp;
+
+		String[] str_arr 	 = line.trim().split("\\s+"); 	// Split by white space
+
+		for(String itr : str_arr){
+			curr 		= itr.charAt(0);
+
+			if(varh.isInt(itr)){
+				if(flag == 1){
+					throw new Exception("2 adjacent ints detected");
+				}
+				flag = 1;
+			}
+			else if(oper.isOperand(curr)){
+				if(oper.checkLeftBrac(curr) && flag != 1){
+					stk.push(curr);
+
+				}else if(flag == 0){
+					throw new Exception("2 adjacent operands detected");
+
+				}else if(oper.checkRightBrac(curr)){
+					if(stk.empty()) throw new Exception("Failure for bracket closure");
+					stk.pop();
+					flag = 1;
+
+				}else{
+					flag = 0;
+
+				}
+			}
+			else{
+				throw new Exception("Invalid input detected");
+			}
+		}
+		return true;
+	}
+
+	public int parseExpr(String line){
 		String[] str_arr 	= line.trim().split("\\s+"); 	// Split by white space
+		char op_curr, op_tmp;
+		int var_A, var_B;
+
 		for(String itr : str_arr){
 			if(varh.isInt(itr)){
-				// Start parsing intger inputs
-				varh.pushStack(itr); 
+				varh.pushStack(itr); 	
 			}else if(oper.isOperand(itr.charAt(0))){
-				// Start parsing operands
+				op_curr 	= itr.charAt(0);
+
+				// Right bracket encountered
+				if(oper.checkRightBrac(op_curr)){
+				}
+
 				oper.pushStack(itr.charAt(0));
-			}else{
-				throw new Exception("Invalid inputs detected.");
 			}
 			// System.out.println(itr);
 		}
@@ -44,6 +89,7 @@ public class EqEval{
 		EqEval eq_eval = new EqEval();
 		try{
 			String formated_line = eq_eval.formatString(line);
+			eq_eval.assertValidString(formated_line);
 			eq_eval.parseExpr(formated_line);
 		}catch(Exception ex){
 			ex.printStackTrace(new PrintStream(System.out));
