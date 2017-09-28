5845c79d4f482a09a4040f40267ae05085c38c79
Johan <JKOK005@e.ntu.edu.sg>
2017-09-28 16:56:46 +0800

    Code works. Time to refractor
    Initial tests on the code seems to suggest that it works. Moving on to
    refractoring and making the code look tidier.

--- leet code/Reverse nodes in K group/Solutions.java
+++ leet code/Reverse nodes in K group/Solutions.java
@@ -5,38 +5,67 @@ import java.io.*;
 import java.lang.*;
 
 public class Solutions{
-	private static Node cur_ptr;
-	private static Node nxt_ptr;
-	private static Node new_root = null;
-	private static Node root 	= null;
+	private static Node cur_ptr 	= null;
+	private static Node head_ptr 	= null;
+	private static Node nxt_ptr 	= null;
+	private static Node new_root 	= null;
+
 	private static int k_max;
+	private static int counter;
+	private static int quotient;
+	private static int remainder;
+	private static boolean flag 	= false;
 
-	private Node reverse(Node head, int K){
-		if(head == null) return head;
-
-		if(K == 1){
-			if(Solutions.new_root == null) Solutions.new_root = head;
-			Solutions.root 		= head;
-			Solutions.cur_ptr 	= head;
-			Solutions.nxt_ptr 	= head.getNext();
+	private void reverse(Node head, int count){
+		if(head != null){
+			reverse(head.getNext(), count +1);
 		}else{
-			reverse(head.getNext(), K -1);
-			Solutions.cur_ptr.setNext(head);
-			Solutions.cur_ptr = head;
+			return;
 		}
 
-		if(K == Solutions.k_max){
-			Solutions.root 	= Solutions.nxt_ptr;
-			head.setNext(reverse(Solutions.nxt_ptr, Solutions.k_max));
-			System.out.println(String.format("%d ", head.getNext().getValue()));
+		if(!Solutions.flag){
+			Solutions.quotient 		= count / Solutions.k_max;
+			Solutions.remainder 	= count - Solutions.quotient *Solutions.k_max;
+			Solutions.flag 			= true;
 		}
-		// System.out.println(String.format("%d ", root.getValue()));
-		return Solutions.root;		
+
+		if(Solutions.remainder > 1){
+			Solutions.remainder 	-= 1;
+			return;
+		}else if(Solutions.remainder == 1){
+			Solutions.nxt_ptr 		= head;
+			Solutions.remainder 	-= 1;
+			return;
+		}
+
+		if(Solutions.quotient <= 1 && Solutions.new_root == null){
+			Solutions.new_root 	= head;
+		}
+
+		if(Solutions.counter == Solutions.k_max){
+			Solutions.counter 	-= 1;
+			Solutions.head_ptr 	= head;
+			Solutions.cur_ptr 	= head;
+			return;
+		}else if(Solutions.counter > 1){
+			Solutions.counter 	-= 1;
+			Solutions.cur_ptr.setNext(head);
+			Solutions.cur_ptr 	= head;
+			return;
+		}else{
+			Solutions.counter 	= Solutions.k_max;
+			Solutions.cur_ptr.setNext(head);
+			head.setNext(Solutions.nxt_ptr);
+			Solutions.nxt_ptr 	= Solutions.head_ptr;
+			Solutions.quotient 	-= 1;
+			return;
+		}	
 	}
 
 	public Node reverseKGroup(Node head, int k_max){
 		Solutions.k_max 	= k_max;
-		reverse(head, k_max);
+		Solutions.counter 	= k_max;
+		reverse(head, 1);
 		return Solutions.new_root;
 	}
 
@@ -44,7 +73,7 @@ public class Solutions{
 		Node root 		= ListUtils.makeAscendingList(0, 10, 2);
 		ListUtils.print(root);
 		Solutions sol 	= new Solutions();
-		Node new_root 	= sol.reverseKGroup(root, 2);
+		Node new_root 	= sol.reverseKGroup(root, 10);
 		ListUtils.print(new_root);
 	}
 }
