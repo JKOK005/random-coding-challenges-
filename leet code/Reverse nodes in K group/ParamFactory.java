0100ddae7f65f3d6a98eaa127266ac2669bf5051
Johan <JKOK005@e.ntu.edu.sg>
2017-09-28 17:59:52 +0800

    Completed refractoring
    TODO:
    - Add readme description
    - Submit to LeetCode for evaluation

---
+++ leet code/Reverse nodes in K group/ParamFactory.java
@@ -0,0 +1,32 @@
+package LeetCode.ReverseKNodes;
+
+public class ParamFactory{
+	private static ParamFactory param_factory_obj = null;
+	
+	public Node cur_ptr 	= null;
+	public Node head_ptr 	= null;
+	public Node nxt_ptr 	= null;
+	
+	public int k_max;
+	public int counter;
+	public int quotient;
+	public int remainder;
+	public boolean flag 	= false;
+
+	private ParamFactory(){}
+
+	public static ParamFactory get(){
+		if(ParamFactory.param_factory_obj == null){
+			ParamFactory.param_factory_obj 	= new ParamFactory();
+		}
+		return ParamFactory.param_factory_obj;
+	}
+
+	public void setK_max(int val){
+		this.k_max 		= val;
+	}
+
+	public void setCounter(int val){
+		this.counter 	= val;
+	}
+}
--- leet code/Reverse nodes in K group/Solutions.java
+++ leet code/Reverse nodes in K group/Solutions.java
@@ -5,16 +5,8 @@ import java.io.*;
 import java.lang.*;
 
 public class Solutions{
-	private static Node cur_ptr 	= null;
-	private static Node head_ptr 	= null;
-	private static Node nxt_ptr 	= null;
+	private ParamFactory params 	= ParamFactory.get();
 	private static Node new_root 	= null;
-
-	private static int k_max;
-	private static int counter;
-	private static int quotient;
-	private static int remainder;
-	private static boolean flag 	= false;
 
 	private void reverse(Node head, int count){
 		if(head != null){
@@ -23,57 +15,54 @@ public class Solutions{
 			return;
 		}
 
-		if(!Solutions.flag){
-			Solutions.quotient 		= count / Solutions.k_max;
-			Solutions.remainder 	= count - Solutions.quotient *Solutions.k_max;
-			Solutions.flag 			= true;
+		if(!params.flag){
+			params.quotient 	= count / params.k_max;
+			params.remainder 	= count - params.quotient *params.k_max;
+			params.flag 		= true;
 		}
 
-		if(Solutions.remainder > 1){
-			Solutions.remainder 	-= 1;
-			return;
-		}else if(Solutions.remainder == 1){
-			Solutions.nxt_ptr 		= head;
-			Solutions.remainder 	-= 1;
+		if(params.remainder >= 1){
+			if(params.remainder == 1) params.nxt_ptr = head;
+			params.remainder 	-= 1;
 			return;
 		}
 
-		if(Solutions.quotient <= 1 && Solutions.new_root == null){
+		if(params.quotient <= 1 && Solutions.new_root == null){
 			Solutions.new_root 	= head;
 		}
 
-		if(Solutions.counter == Solutions.k_max){
-			Solutions.counter 	-= 1;
-			Solutions.head_ptr 	= head;
-			Solutions.cur_ptr 	= head;
-			return;
-		}else if(Solutions.counter > 1){
-			Solutions.counter 	-= 1;
-			Solutions.cur_ptr.setNext(head);
-			Solutions.cur_ptr 	= head;
-			return;
-		}else{
-			Solutions.counter 	= Solutions.k_max;
-			Solutions.cur_ptr.setNext(head);
-			head.setNext(Solutions.nxt_ptr);
-			Solutions.nxt_ptr 	= Solutions.head_ptr;
-			Solutions.quotient 	-= 1;
-			return;
+		if(params.counter > 1){
+			if(params.counter == params.k_max){
+				params.head_ptr = head;
+			}else{
+				params.cur_ptr.setNext(head);
+			}
+			params.cur_ptr 	= head;
+			params.counter 	-= 1;
+		}
+		else{
+			params.counter 	= params.k_max;
+			params.cur_ptr.setNext(head);
+			head.setNext(params.nxt_ptr);
+			params.nxt_ptr 	= params.head_ptr;
+			params.quotient 	-= 1;
 		}	
+		return;
 	}
 
 	public Node reverseKGroup(Node head, int k_max){
-		Solutions.k_max 	= k_max;
-		Solutions.counter 	= k_max;
+		if(k_max <= 1) return head;
+		params.setK_max(k_max);
+		params.setCounter(k_max);
 		reverse(head, 1);
 		return Solutions.new_root;
 	}
 
 	public static void main(String[] args){
-		Node root 		= ListUtils.makeAscendingList(0, 10, 2);
+		Node root 		= ListUtils.makeAscendingList(0, 20, 1);
 		ListUtils.print(root);
 		Solutions sol 	= new Solutions();
-		Node new_root 	= sol.reverseKGroup(root, 10);
+		Node new_root 	= sol.reverseKGroup(root, 3);
 		ListUtils.print(new_root);
 	}
 }
