3c0177d652aabb0b6e1a06f3e6f2ad9a45622100
Johan <JKOK005@e.ntu.edu.sg>
2017-11-07 01:13:09 +0800

    Solved Longest Valid Parenthesis
    - Solution accepted by Leet Code
    
    Todo:
    - Change string input to text file stream
    - Add JUnit test cases
    - Complete readme doc

---
+++ leet code/longest valid parenthesis/Solutions.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+
+public class Solutions{
+	public void printArr(boolean[] arr){
+		for(int i =0; i < arr.length; i++){
+			if(arr[i]) System.out.print(1);
+			else System.out.print(0);
+		}
+	}
+
+	private int getMaxLength(boolean[] arr){
+		int maxLength 	= 0;
+		int tmp 		= 0;
+		for(int i =0; i < arr.length; i++){
+			if(arr[i]){
+				tmp++;
+			}else{
+				maxLength 	= Math.max(maxLength, tmp);
+				tmp 		= 0;
+			}
+		}
+		return Math.max(maxLength, tmp);
+	}
+
+	public int longestValidParentheses(String str){
+		boolean[] validBracArray 		= new boolean[str.length()];
+		Stack<Integer> leftBracStack 	= new Stack<Integer>();
+
+		for(int i =0; i < str.length(); i++){
+			char cur 	= str.charAt(i);
+			if(cur == '('){
+				leftBracStack.push(i);
+			}else if(!leftBracStack.empty()){
+				int start 	= leftBracStack.pop();
+				validBracArray[start] 	= true;
+				validBracArray[i] 		= true;
+			}	
+		}
+		return getMaxLength(validBracArray);
+	}
+
+	public static void main(String[] args){
+		Solutions sol 	= new Solutions();
+		int maxLength 	= sol.longestValidParentheses(")(())(()(())");
+		System.out.println(maxLength);
+	}
+}
---
+++ leet code/longest valid parenthesis/readme.md
@@ -0,0 +1,20 @@
+## Longest valid parenthesis
+### Reference can be found in this [link from LeetCode](https://leetcode.com/problems/longest-valid-parentheses/description/)
+
+### Solution discussion
+Initial commit - To be updated
+
+### Compile and run
+Compile the main script and all dependencies
+```
+javac Solutions.java -d .
+```
+
+Run the Solutions script in the package
+```
+java Solutions
+```
+
+### Complexity
+Time complexity 	: O(N), N is the length of input string
+Space Complexity 	: O(N)
