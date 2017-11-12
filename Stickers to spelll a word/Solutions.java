57a815b7c9eea7c0bf522a3a6943839b12a3c93c
Johan <JKOK005@e.ntu.edu.sg>
2017-11-12 21:11:28 +0800

    Implemented algorithm without test
    - Algorithm passes 66 / 100 test cases on Leet code
    - Issue seems to be the long run time for many stickers
    	- 90 ms > 35 ms allowable runtime
    
    TODO:
    - Reduce run time of the algorithm
    - Develop test cases for the programme

--- Stickers to spelll a word/Solutions.java
+++ Stickers to spelll a word/Solutions.java
@@ -3,10 +3,10 @@ import java.io.*;
 import java.lang.*;
 
 public class Solutions{
-	private HashMap<String, int> stringToMinStickersMap;
+	private HashMap<String, Integer> stringToMinStickersMap;
 
 	public Solutions(){
-		stringToMinStickersMap 	= new HashMap<String, int>();
+		stringToMinStickersMap 	= new HashMap<String, Integer>();
 	}
 
 	public void setStringToMinStickersMap(String str, int x){
@@ -21,26 +21,46 @@ public class Solutions{
 		return this.stringToMinStickersMap.get(key);
 	}
 
-	public String sortString(String str){
-		return Arrays.sort(str);
+	private String sortString(String str){
+		char[] toSort 	= str.toCharArray();
+		Arrays.sort(toSort);
+		return String.valueOf(toSort);
 	}
 
-	public String removeString(String toSubtract, String original){
+	private String removeCharFromString(char c, String str){
+		String charToString = Character.toString(c);
+		return str.replaceFirst(charToString, "");
+	}
+
+	private String removeString(String toSubtract, String original){
 		String newStr = original;
-		for(String eachChar : toSubtract){
-			newStr = newStr.replaceFirst(eachChar, "");
+		for(char eachChar : toSubtract.toCharArray()){
+			newStr = this.removeCharFromString(eachChar, newStr);
 		}
 		return newStr;
 	}
 
-	public int getMinStickers(String[] stickers, String sortedTarget){
+	private boolean possibleToFormWord(String[] stickers, String target){
+		HashSet<Character> hs 	= new HashSet<Character>();
+		for(String sticker : stickers){
+			for(char eachChar : sticker.toCharArray()){
+				hs.add(eachChar);
+			}
+		}
+		for(char eachChar : target.toCharArray()){
+			if(!hs.contains(eachChar)) return false;
+		}
+		return true;
+	}
+
+	private int getMinStickers(String[] stickers, String sortedTarget){
 		if(this.hasKey(sortedTarget)){
 			return this.getValueFromKey(sortedTarget);
 		}else if(sortedTarget.equals("")){
 			return 0;
 		}
 
-		int minCount 	= INT_MAX;
+		int minCount = Integer.MAX_VALUE;
 
 		for(String sticker : stickers){
 			String subString = this.removeString(sticker, sortedTarget);
@@ -53,14 +73,20 @@ public class Solutions{
 	}
 
 	public int minStickers(String[] stickers, String target){
-		// Check if we can get all alphaberts of the target, else return -1
-
+		if(!this.possibleToFormWord(stickers, target)) return -1;
 		String sortedTarget = this.sortString(target);
 		this.getMinStickers(stickers, sortedTarget);
 		return this.getValueFromKey(sortedTarget);
 	}
 
 	public static void main(String[] args){
-
+		String[] stickers 	= {"letter","gold","give","fish","them","system","four","say","sudden","laugh","dear","still","boat","apple","thought","horse","from","prepare","how","provide","pay","people","three","to","hot","card","above","save","division","notice","complete","rain","band","game","mouth","decide","segment","him","bank","cool","term","life","against","motion","edge","nose","clean","enter","operate","where"};
+		String target 		= "lessremember";
+		Solutions sol 		= new Solutions();
+		long startTime = System.currentTimeMillis();
+		System.out.println(sol.minStickers(stickers, target));
+		long endTime   = System.currentTimeMillis();
+		long totalTime = endTime - startTime;
+		System.out.println(totalTime);
 	}
 }
